ae900b51e16336266b3be5e8a6087d51b415606c
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2020-03-16 20:18:21 +0100

    Update Login.js

--- app/routes/Auth/Login/Login.js
+++ app/routes/Auth/Login/Login.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from "react-router-dom";
 import {ChangeLanguage} from "../../components/Dropdowns/ChangeLanguage";
 
-import {
-    Button,
-    CustomInput,
-    EmptyLayout,
-    Form,
-    FormGroup,
-    FormText,
-    Input,
-    Label,
-    ThemeConsumer
-} from './../../../components';
-
+import {CustomInput, EmptyLayout, FormGroup, ThemeConsumer} from './../../../components';
+import {Button, Form, InputGroup,} from 'react-bootstrap';
 import {HeaderAuth} from "../../components/Pages/HeaderAuth";
 import {FooterAuth} from "../../components/Pages/FooterAuth";
 import {useTranslation} from "react-i18next";
+import {Formik} from 'formik';
 
 export default function Login() {
+    let history = useHistory();
     const {t} = useTranslation();
     return (
         <EmptyLayout>
@@ -27,34 +19,108 @@ export default function Login() {
                 <HeaderAuth title={t('Login.title')} subtitle={t('Login.subtitle')}/>
                 { /* END Header */}
                 { /* START Form */}
-                <Form action="/" className="mb-3">
-                    <FormGroup>
-                        <Label for="emailAdress">{t('Login.email')}</Label>
-                        <Input type="email" name="email" id="emailAdress" placeholder={t('Login.emailPlaceholder')}
-                               className="bg-white"/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="password">{t('Login.password')}</Label>
-                        <Input type="password" name="password" id="password"
-                               placeholder={t('Login.passwordPlaceholder')} className="bg-white"/>
-                        <FormText color="muted">{t('Login.passwordText')}</FormText>
-                    </FormGroup>
-                    <FormGroup>
-                        <CustomInput type="checkbox" id="rememberPassword" label={t('Login.rememberMe')} inline/>
-                    </FormGroup>
-                    <ThemeConsumer>
-                        {
-                            ({color}) => (
-                                <Button color={color} block tag={Link} to="/">{t('Login.signIn')}</Button>
-                            )
-                        }
-                    </ThemeConsumer>
-                </Form>
+                <Formik
+                    isSubmitting={false}
+                    onSubmit={(values, actions) => {
+                        fetch('http://localhost:3000/user/login', {
+                            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                            credentials: 'same-origin', // include, *same-origin, omit
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            redirect: 'follow', // manual, *follow, error
+                            referrerPolicy: 'no-referrer', // no-referrer, *client
+                            body: JSON.stringify(values) // body data type must match "Content-Type" header
+                        })
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    console.log(result);
+                                    if (result.token) {
+                                        localStorage.setItem('auth', result.token);
+                                        setTimeout(() => {
+                                            actions.setSubmitting(false);
+                                            history.push('/dashboards/analytics');
+                                        }, 3000);
+                                    }
+                                    localStorage.setItem('auth', result.token);
+                                },
+                                (error) => {
+                                    console.log(error);
+                                    setTimeout(() => {
+                                        -
+                                            actions.setSubmitting(false);
+                                    }, 3000);
+                                }
+                            );
+                        setTimeout(() => {
+                            actions.setSubmitting(false);
+                        }, 3000);
+                    }}
+                    initialValues={{}}>
+                    {({
+                          handleSubmit,
+                          values,
+                          isSubmitting,
+                          handleChange
+                      }) => (
+                        <Form noValidate onSubmit={handleSubmit} className="mb-3">
+                            <Form.Group>
+                                <Form.Label>{t('Login.email')}</Form.Label>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder={t('Login.emailPlaceholder')}
+                                        aria-describedby="inputGroupPrepend"
+                                        name="email"
+                                        className="bg-white"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                    />
+                                </InputGroup>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>{t('Register.password')}</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder={t('Login.passwordPlaceholder')}
+                                    aria-describedby="inputGroupPrepend"
+                                    name="password"
+                                    className="bg-white"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                />
+                                <Form.Text color="muted"> {t('Login.passwordText')}</Form.Text>
+                            </Form.Group>
+                            <FormGroup>
+                                <CustomInput type="checkbox"
+                                             id="rememberPassword"
+                                             label={t('Login.rememberMe')}
+                                             i
+                                             inline/>
+                            </FormGroup>
+                            <ThemeConsumer>
+                                {
+                                    ({color}) => (
+                                        <Button color={color} block
+                                                type="submit"
+                                                disabled={isSubmitting}
+                                        >{t('Register.signUp')}</Button>
+                                    )
+                                }
+                            </ThemeConsumer>
+                        </Form>
+                    )}
+                </Formik>
                 { /* END Form */}
                 { /* START Bottom Links */}
                 <div className="d-flex mb-5">
-                    <Link to="/pages/forgotpassword" className="text-decoration-none">{t('Login.forgotPassword')}</Link>
-                    <Link to="/pages/register" className="ml-auto text-decoration-none">{t('Login.Register')}</Link>
+                    <Link to="/auth/forgotpassword" className="text-decoration-none">{t('Login.forgotPassword')}</Link>
+                    <Link to="/auth/register" className="ml-auto text-decoration-none">{t('Login.Register')}</Link>
                 </div>
                 { /* END Bottom Links */}
                 { /* START Footer */}
