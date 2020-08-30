d32c9ed83ff31fc728a7880a7e543c5d1b4e5d94
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-08-30 16:02:09 +0200

    Create index.js

---
+++ Backend/config/index.js
@@ -0,0 +1,7 @@
+import tokenConfig from './token.config';
+import serverConfig from './server.config';
+import mailConfig from './mail.config';
+import databaseConfig from './database.config';
+
+
+export default {tokenConfig, serverConfig, mailConfig, databaseConfig};
