6d7bb11ad9a87d09a736e0d756deeb6775adadba
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-08-30 16:02:32 +0200

    Create token.config.js

---
+++ Backend/config/token.config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const secretAccess = process.env.SECRET_TOKEN_ACCESS;
+const expireAccess = process.env.EXPIRE_TOKEN_ACCESS;
+
+const secretRefresh = process.env.SECRET_TOKEN_REFRESH;
+const expireRefresh = process.env.EXPIRE_TOKEN_REFRESH;
+
+const secretRestore = process.env.SECRET_TOKEN_RESTORE_PASSWORD;
+const expireRestore = process.env.EXPIRE_TOKEN_RESTORE_PASSWORD;
+
+const countTokenLimit = process.env.TOKEN_LIMIT_COUNT_DIVICE;
+
+export default {
+    secretAccess,
+    expireAccess,
+    secretRefresh,
+    expireRefresh,
+    countTokenLimit,
+    secretRestore,
+    expireRestore
+};
+
