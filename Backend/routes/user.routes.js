0790b596b6ab728e52195abb60956c751a26d7b4
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-09-14 19:56:43 +0200

    Update user.routes.js

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,4 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
+import UserController from "../controllers/user.controller";
+import AuthorizeMiddleware from "../middleware/authorize.middleware";
+
 const router = Router();
+
+router.post(
+  "/user/update/settings",
+  AuthorizeMiddleware.check_access,
+  UserController.updateSettings
+);
 
 export default router;
