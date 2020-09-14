15fd7e575f2e844872b9e3e17c62b3173a07265a
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-09-14 19:47:13 +0200

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,30 @@
-class UserController{
+import UserModel from "../models/user.model";
+import ClientError from "../exeptions/client.error";
+import TryCatchErrorDecorator from "../decorators/TryCatchError.decorator";
+import _ from "lodash";
 
+class UserController {
+  @TryCatchErrorDecorator
+  static async updateSettings(req, res) {
+    const user = await UserModel.findOne({ _id: req.userId });
+    const settings = req.body.settings;
+    if (!settings) {
+      throw new ClientError(
+        res,
+        "settings is required to update user settings",
+        401
+      );
+    }
+    user.data.settings = settings;
+    await user.save();
+
+    user.webTokens = [];
+    user.password = "";
+
+    res.json({
+      user,
+    });
+  }
 }
+
 export default UserController;
