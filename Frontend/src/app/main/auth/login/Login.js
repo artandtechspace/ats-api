bad0d41c7b91680a98b08b251b15b6d556c1f4b0
Luca Schöneberg <luca-schoeneberg@outlook.com>
2020-09-17 10:50:20 +0200

    Update Login.js

--- Frontend/src/app/main/auth/login/Login.js
+++ Frontend/src/app/main/auth/login/Login.js
@@ -15,17 +15,10 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		background: `linear-gradient(to left, ${theme.palette.primary.dark} 0%, ${darken(
-			theme.palette.primary.dark,
-			0.5
-		)} 100%)`,
-		color: theme.palette.primary.contrastText
-	},
-	leftSection: {},
-	rightSection: {
 		background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
 			theme.palette.primary.dark,
 			0.5
@@ -66,157 +59,136 @@ function Login() {
 	}
 
 	return (
-		<div
-			className={clsx(
-				classes.root,
-				'flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24'
-			)}
-		>
-			<FuseAnimate animation="transition.expandIn">
-				<div className="flex w-full max-w-400 md:max-w-3xl rounded-12 shadow-2xl overflow-hidden">
-					<Card
-						className={clsx(
-							classes.leftSection,
-							'flex flex-col w-full max-w-sm items-center justify-center'
-						)}
-						square
-						elevation={0}
-					>
-						<CardContent className="flex flex-col items-center justify-center w-full py-96 max-w-320">
-							<FuseAnimate delay={300}>
-								<div className="flex items-center mb-32">
-									<img className="logo-icon w-48" src="assets/images/logos/fuse.svg" alt="logo" />
-									<div className="border-l-1 mr-4 w-1 h-40" />
-									<div>
-										<Typography className="text-24 font-800 logo-text" color="inherit">
-											FUSE
-										</Typography>
-										<Typography
-											className="text-16 tracking-widest -mt-8 font-700"
-											color="textSecondary"
-										>
-											REACT
-										</Typography>
-									</div>
-								</div>
-							</FuseAnimate>
+		<div className={clsx(classes.root, 'flex flex-col flex-auto flex-shrink-0 p-24 md:flex-row md:p-0')}>
+			<div className="flex flex-col flex-grow-0 items-center text-white p-16 text-center md:p-128 md:items-start md:flex-shrink-0 md:flex-1 md:text-left">
+				<FuseAnimate animation="transition.expandIn">
+					<img className="w-128 mb-32" src="assets/images/logos/fuse.svg" alt="logo" />
+				</FuseAnimate>
 
-							<div className="w-full">
-								<Formsy
-									onValidSubmit={handleSubmit}
-									onValid={enableButton}
-									onInvalid={disableButton}
-									ref={formRef}
-									className="flex flex-col justify-center w-full"
-								>
-									<TextFieldFormsy
-										className="mb-16"
-										type="text"
-										name="email"
-										label="Username/Email"
-										value="lucaq@awd.co"
-										validations={{
-											minLength: 4
-										}}
-										validationErrors={{
-											minLength: 'Min character length is 4'
-										}}
-										InputProps={{
-											endAdornment: (
-												<InputAdornment position="end">
-													<Icon className="text-20" color="action">
-														email
-													</Icon>
-												</InputAdornment>
-											)
-										}}
-										variant="outlined"
-										required
-									/>
+				<FuseAnimate animation="transition.slideUpIn" delay={300}>
+					<Typography variant="h3" color="inherit" className="font-light">
+						Welcome to the FUSE!
+					</Typography>
+				</FuseAnimate>
 
-									<TextFieldFormsy
-										className="mb-16"
-										type="password"
-										name="password"
-										label="Password"
-										value="L!23123adawAW"
-										validations={{
-											minLength: 4
-										}}
-										validationErrors={{
-											minLength: 'Min character length is 4'
-										}}
-										InputProps={{
-											className: 'pr-2',
-											type: showPassword ? 'text' : 'password',
-											endAdornment: (
-												<InputAdornment position="end">
-													<IconButton onClick={() => setShowPassword(!showPassword)}>
-														<Icon className="text-20" color="action">
-															{showPassword ? 'visibility' : 'visibility_off'}
-														</Icon>
-													</IconButton>
-												</InputAdornment>
-											)
-										}}
-										variant="outlined"
-										required
-									/>
-									<div className="flex items-center justify-between">
-										<CheckboxFormsy name="remember" label="Remember Me" />
-										<Link className="font-medium" to="/forgot-password">
-											Forgot Password?
-										</Link>
-									</div>
+				<FuseAnimate delay={400}>
+					<Typography variant="subtitle1" color="inherit" className="max-w-512 mt-16">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper nisl erat, vel
+						convallis elit fermentum pellentesque. Sed mollis velit facilisis facilisis.
+					</Typography>
+				</FuseAnimate>
+			</div>
 
-									<Button
-										type="submit"
-										variant="contained"
-										color="primary"
-										className="w-full mx-auto mt-16 normal-case"
-										aria-label="LOG IN"
-										disabled={!isFormValid}
-										value="legacy"
-									>
-										Login
-									</Button>
-								</Formsy>
-							</div>
-						</CardContent>
+			<FuseAnimate animation={{ translateX: [0, '100%'] }}>
+				<Card className="w-full max-w-400 mx-auto m-16 md:m-0" square>
+					<CardContent className="flex flex-col items-center justify-center p-32 md:p-48 md:pt-128 ">
+						<Typography variant="h6" className="md:w-full mb-32">
+							LOGIN TO YOUR ACCOUNT
+						</Typography>
 
-						<div className="flex flex-col items-center justify-center pb-32">
-							<div>
-								<span className="font-medium mr-8">Don't have an account?</span>
-								<Link className="font-medium" to="/register">
-									Register
+						<Formsy
+							onValidSubmit={handleSubmit}
+							onValid={enableButton}
+							onInvalid={disableButton}
+							ref={formRef}
+							className="flex flex-col justify-center w-full"
+						>
+							<TextFieldFormsy
+								className="mb-16"
+								type="text"
+								name="email"
+								label="Username/Email"
+								value="lucaq@awd.co"
+								validations={{
+									minLength: 4
+								}}
+								validationErrors={{
+									minLength: 'Min character length is 4'
+								}}
+								InputProps={{
+									endAdornment: (
+										<InputAdornment position="end">
+											<Icon className="text-20" color="action">
+												email
+											</Icon>
+										</InputAdornment>
+									)
+								}}
+								variant="outlined"
+								required
+							/>
+
+							<TextFieldFormsy
+								className="mb-16"
+								type="password"
+								name="password"
+								label="Password"
+								value="L!23123adawAW"
+								validations={{
+									minLength: 4
+								}}
+								validationErrors={{
+									minLength: 'Min character length is 4'
+								}}
+								InputProps={{
+									className: 'pr-2',
+									type: showPassword ? 'text' : 'password',
+									endAdornment: (
+										<InputAdornment position="end">
+											<IconButton onClick={() => setShowPassword(!showPassword)}>
+												<Icon className="text-20" color="action">
+													{showPassword ? 'visibility' : 'visibility_off'}
+												</Icon>
+											</IconButton>
+										</InputAdornment>
+									)
+								}}
+								variant="outlined"
+								required
+							/>
+
+							<div className="flex items-center justify-between">
+								<CheckboxFormsy name="remember" label="Remember Me" />
+								<Link className="font-medium" to="/forgot-password">
+									Forgot Password?
 								</Link>
 							</div>
-							<Link className="font-medium mt-8" to="/">
-								Back to Dashboard
+
+							<Button
+								type="submit"
+								variant="contained"
+								color="primary"
+								className="w-full mx-auto mt-16"
+								aria-label="LOG IN"
+								disabled={!isFormValid}
+								value="legacy"
+							>
+								LOGIN
+							</Button>
+						</Formsy>
+
+						<div className="my-24 flex items-center justify-center">
+							<Divider className="w-32" />
+							<span className="mx-8 font-bold">OR</span>
+							<Divider className="w-32" />
+						</div>
+
+						<Button variant="contained" color="secondary" size="small" className="normal-case w-192 mb-8">
+							Log in with GitHub
+						</Button>
+
+						<Button variant="contained" color="primary" size="small" className="normal-case w-192">
+							Log in with Apple
+						</Button>
+
+						<div className="flex flex-col items-center justify-center pt-32 pb-24">
+							<span className="font-medium">Don't have an account?</span>
+							<Link className="font-medium" to="/pages/auth/register-2">
+								Create an account
 							</Link>
 						</div>
-					</Card>
-
-					<div
-						className={clsx(classes.rightSection, 'hidden md:flex flex-1 items-center justify-center p-64')}
-					>
-						<div className="max-w-320">
-							<FuseAnimate animation="transition.slideUpIn" delay={400}>
-								<Typography variant="h3" color="inherit" className="font-800 leading-tight">
-									Welcome <br />
-									to the <br /> FUSE React!
-								</Typography>
-							</FuseAnimate>
-
-							<FuseAnimate delay={500}>
-								<Typography variant="subtitle1" color="inherit" className="mt-32">
-									Powerful and professional admin template for Web Applications, CRM, CMS, Admin
-									Panels and more.
-								</Typography>
-							</FuseAnimate>
-						</div>
-					</div>
-				</div>
+					</CardContent>
+				</Card>
 			</FuseAnimate>
 		</div>
 	);
