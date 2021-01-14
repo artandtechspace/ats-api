dc99614d043d03327b5549866a4f604ff1524cde
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-01-14 09:16:06 +0100

    add revoke middleware to permissioner

--- app/middleware/permissioner.js
+++ app/middleware/permissioner.js
@@ -1,7 +1,6 @@
 const permModel = require('../models/permission')
-const userModel = require('../models/user')
 const {itemAlreadyExists, itemNotFound} = require('../middleware/utils')
-const db = require('../middleware/db')
+const mongoose = require('mongoose')
 
 module.exports = {
     async permissionExists(permission) {
@@ -29,10 +28,33 @@ module.exports = {
         })
     },
 
+    async permissionIdIsLinkAssigned(user, id) {
+        return new Promise((resolve, reject) => {
+            user = JSON.parse(JSON.stringify(user))
+            const item = user.permissions.find(buffer => buffer._id === id);
+            itemNotFound(null, item, reject, 'PERMISSION_LINK_IS_NOT_ASSIGNED')
+            resolve(true)
+        })
+    },
+
     async permissionIsAssigned(user, id) {
         return new Promise((resolve, reject) => {
-            const item = user.permissions.find(buffer=>buffer.permissionId === id);
+            const item = user.permissions.find(buffer => buffer.permissionId === id);
             itemAlreadyExists(null, item, reject, 'PERMISSION_ALREADY_ASSIGNED')
+            resolve(true)
+        })
+    },
+
+    async permissionIsRevokeActive(user, id) {
+        return new Promise((resolve, reject) => {
+            const item = user.permissionsRevoke.filter(buffer => buffer.permissionIdLink === id);
+            if (item) {
+                item.forEach(revoke => {
+                    if (revoke.revokeIsActive) {
+                        itemAlreadyExists(null, item, reject, 'PERMISSION_REVOKE_ALREADY_ASSIGNED')
+                    }
+                })
+            }
             resolve(true)
         })
     }
