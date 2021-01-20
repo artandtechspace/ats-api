67f956b374dedd0d17cfe67b4d300cfd0d89802d
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-01-20 23:03:16 +0100

    do some temporary things

--- app/routes/userPermissionAccess.js
+++ app/routes/userPermissionAccess.js
@@ -1,5 +1,5 @@
-const controller = require('../controllers/userPermission')
-const validate = require('../controllers/userPermission.validate')
+const controller = require('../controllers/userPermissionAccess')
+const validate = require('../controllers/userPermissionAccess')
 const AuthController = require('../controllers/auth')
 const express = require('express')
 const router = express.Router()
@@ -9,9 +9,37 @@ const requireAuth = passport.authenticate('jwt', {
     session: false
 })
 const trimRequest = require('trim-request')
-
+const {validationResult} = require("../middleware/utils");
+const {check, oneOf} = require('express-validator')
 /*
  * UserPermissionAccess routes
  */
+
+router.post(
+    '/',
+    requireAuth,
+    AuthController.roleAuthorization(['user', 'admin']),
+    trimRequest.all,
+    oneOf([
+        check('permissionId')
+            .exists()
+            .withMessage('MISSING')
+            .not()
+            .isEmpty()
+            .withMessage('IS_EMPTY')
+            .isMongoId()
+            .withMessage('USERID_NOT_MONGOID'),
+        check('permission')
+            .exists()
+            .withMessage('MISSING')
+            .not()
+            .isEmpty()
+            .withMessage('IS_EMPTY'),
+    ]),
+    (req, res, next) => {
+        validationResult(req, res, next)
+    },
+    controller.createItem
+)
 
 module.exports = router
