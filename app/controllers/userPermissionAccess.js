73320ca1879d2d46a00d52991356192c6319914d
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-01-20 23:02:43 +0100

    rewrite and bugfixing

--- app/controllers/userPermissionAccess.js
+++ app/controllers/userPermissionAccess.js
@@ -16,7 +16,7 @@ const db = require('../middleware/db')
 const createAccessItem = async req => {
     return new Promise((resolve, reject) => {
         const userPermissionsAccess = new userPermissionsAccessModel({
-            userid: req.id,
+            userid: req.userid,
             userNameCache: req.userNameCache,
             permissionId: req.permissionId,
             permissionNameCache: req.permissionNameCache
@@ -45,16 +45,19 @@ const findUserById = async userId => {
 
 exports.createItem = async (req, res) => {
     try {
-        const data = matchedData(req)
-        const user = findUserById(data.id)
-        if (data.permmissionId) data.permmissionLink = await permissioner.permissionIsAssigned(user, data.permmissionId)
-        else if (data.permmission) {
-            data.permmissionId = await permissioner.permissionGetId(data.permmission)
-            data.permmissionLink = await permissioner.permissionIsAssigned(user, data.permmissionId)
-        }
-        await permissioner.permissionIsRevokedActive(user, data.permmission, 'PERMISSION_REVOKE_IS_NOT_ASSIGNED', true)
+        const user = await findUserById(req.user._id)
+        const data = matchedData(req);
+        if (data.permissionId !== undefined) data.perm = await permissioner.permissionGetById(data.permissionId)
+        if (data.permission !== undefined) data.perm = await permissioner.permissionGetByName(data.permission)
+        data.userid = user._id
+        data.userNameCache = user.name
+        data.permissionNameCache = data.perm.permission
+        data.permissionId = data.perm._id
+        const permission = await permissioner.permissionIsAssigned(user, data.perm._id,'PERMISSION_IS_NOT_ASSIGNED')
+        await permissioner.permissionIsRevokedActive(user, permission._id, 'PERMISSION_REVOKE_IS_NOT_ASSIGNED', true)
+        //check if Machine is in use(add machiner)
         //unlock Machine
-        const resolve = await createItem(data)
+        const resolve = await createAccessItem(data)
         res.status(201).json(resolve)
     } catch (error) {
         utils.handleError(res, error)
@@ -63,7 +66,8 @@ exports.createItem = async (req, res) => {
 
 exports.closeItem = async (req, res) => {
     try {
-        const data= {status:"error function not ready"}
+        const data = {status: "error function not ready"}
+        // add everything smile
         res.status(201).json(data)
     } catch (error) {
         utils.handleError(res, error)
