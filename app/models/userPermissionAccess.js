f438a2918aacd89129f4e11c198252bcf8d393c8
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-01-21 16:24:13 +0100

    Update userPermissionAccess.js

--- app/models/userPermissionAccess.js
+++ app/models/userPermissionAccess.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
 
 const UserPermissionAccessSchema = new mongoose.Schema(
     {
@@ -30,4 +31,5 @@ const UserPermissionAccessSchema = new mongoose.Schema(
     }
 )
 
+UserPermissionAccessSchema.plugin(mongoosePaginate)
 module.exports = mongoose.model('UserPermissionAccess', UserPermissionAccessSchema)
