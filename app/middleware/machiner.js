9e8b105faf553f7cac938d842b4707410e5dcfe6
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-01-22 09:39:48 +0100

    Update machiner.js

--- app/middleware/machiner.js
+++ app/middleware/machiner.js
@@ -5,6 +5,12 @@ const axios = require('axios')
 
 module.exports = {
 
+    /**
+     * Check if Machine is active ore not by permission._id
+     * @param {string} permId permissionId - from model permission
+     * @param {string} message - error json message
+     * @param {boolean} invert - inverting return
+     */
     async isMachineActiveByPermId(permId, message, invert = false) {
         return new Promise((resolve, reject) => {
             userPermAccessModel.find({permissionId: permId.toString()})
@@ -18,6 +24,12 @@ module.exports = {
         })
     },
 
+    /**
+     * Check if Machine is active ore not by userPermissionsAccess._id
+     * @param {string} accessId - from model userPermissionsAccess
+     * @param {string} message - error json message
+     * @param {boolean} invert - inverting return
+     */
     async isMachineActiveByAccessId(accessId, message, invert = false) {
         return new Promise((resolve, reject) => {
             userPermAccessModel.find({_id: accessId.toString()})
@@ -31,6 +43,13 @@ module.exports = {
         })
     },
 
+    /**
+     * Send api call to activate a machine
+     * @param {Object} data
+     * @param {string} data.ipaddress - URL for api call
+     * @param {string} data.userId - machine cache
+     * @param {string} data.userName - machine display
+     */
     async activateMachine(data) {
         return new Promise((resolve, reject) => {
             axios.post(data.ipaddress + "/activate", data)
@@ -43,6 +62,13 @@ module.exports = {
         })
     },
 
+    /**
+     * Send api call to deactivate a machine
+     * @param {Object} data
+     * @param {string} data.ipaddress - URL for api call
+     * @param {string} data.userId - machine cache
+     * @param {string} data.userName - machine display
+     */
     async deactivateMachine(data) {
         return new Promise((resolve, reject) => {
             axios.post(data.ipaddress + "/deactivate", data)
