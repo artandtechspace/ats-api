bd0036799322f8c7c8b842efc03815cd0c01176f
Luca Schöneberg <37987646+lucaschoeneberg@users.noreply.github.com>
2021-02-25 23:59:33 +0100

    some utilities updates

--- app/middleware/utils.js
+++ app/middleware/utils.js
@@ -5,39 +5,35 @@ const bson = require('bson')
  * Removes extension from file
  * @param {string} file - filename
  */
-exports.removeExtensionFromFile = file => {
-    return file
-        .split('.')
-        .slice(0, -1)
-        .join('.')
-        .toString()
+exports.removeExtensionFromFile = (file) => {
+    return file.split('.').slice(0, -1).join('.').toString()
 }
 
 /**
  * Gets IP from user
  * @param {*} req - request object
  */
-exports.getIP = req => requestIp.getClientIp(req)
+exports.getIP = (req) => requestIp.getClientIp(req)
 
 /**
  * Gets browser info from user
  * @param {*} req - request object
  */
-exports.getBrowserInfo = req => req.headers['user-agent']
+exports.getBrowserInfo = ({headers}) => headers['user-agent']
 
 /**
  * Gets country from user using CloudFlare header 'cf-ipcountry'
  * @param {*} req - request object
  */
-exports.getCountry = req =>
-    req.headers['cf-ipcountry'] ? req.headers['cf-ipcountry'] : 'XX'
+exports.getCountry = ({headers}) =>
+    headers['cf-ipcountry'] ? headers['cf-ipcountry'] : 'XX'
 
 /**
  * Handles error by printing to console in development env and builds and sends an error response
  * @param {Object} res - response object
  * @param {Object} err - error object
  */
-exports.handleError = (res, err) => {
+exports.handleError = (res = {}, err = {}) => {
     // Prints error in console
     if (process.env.NODE_ENV === 'development') {
         console.log(err)
@@ -52,10 +48,10 @@ exports.handleError = (res, err) => {
 
 /**
  * Builds error object
- * @param {number} code - error code
+ * @param {string} code - error code
  * @param {string} message - error text
  */
-exports.buildErrObject = (code, message) => {
+exports.buildErrObject = (code = '', message = {}) => {
     return {
         code,
         message
@@ -84,7 +80,7 @@ exports.validationResult = (req, res, next) => {
  * Builds success object
  * @param {string} message - success text
  */
-exports.buildSuccObject = message => {
+exports.buildSuccObject = (message = "") => {
     return {
         msg: message
     }
@@ -94,14 +90,10 @@ exports.buildSuccObject = message => {
  * Checks if given ID is good for MongoDB
  * @param {string} id - id to check
  */
-exports.isIDGood = async id => {
+exports.isIDGood = async (id = '') => {
     return new Promise((resolve, reject) => {
-        const goodID = bson.ObjectID.isValid(id)
-        if (true) {
-            return resolve(id)
-        } else {
-            return reject(this.buildErrObject(422, 'ID_MALFORMED'))
-        }
+        const goodID = mongoose.Types.ObjectId.isValid(id)
+        return goodID ? resolve(id) : reject(buildErrObject(422, 'ID_MALFORMED'))
     })
 }
 
@@ -112,13 +104,16 @@ exports.isIDGood = async id => {
  * @param {Object} reject - reject object
  * @param {string} message - message
  */
-exports.itemNotFound = (err, item, reject, message) => {
-    if (err) {
-        reject(this.buildErrObject(422, err.message))
-    }
-    if (!item) {
-        reject(this.buildErrObject(404, message))
-    }
+exports.itemNotFound = (err = {}, item = {}, message = {}) => {
+    return new Promise((resolve, reject) => {
+        if (err) {
+            return reject(buildErrObject(422, err.message))
+        }
+        if (!item) {
+            return reject(buildErrObject(404, message))
+        }
+        resolve()
+    })
 }
 
 /**
